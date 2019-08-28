ba5abef4f80aa1ff8398b52058e6d54aaa2f005d
Boris Shoshin <bober2801@mail.ru>
2019-08-28 19:31:49 +0300

    m

--- Сходимость синуса/example/MySin.js
+++ Сходимость синуса/example/MySin.js
@@ -30,7 +30,7 @@ function addAreaUnderFunc(plot, fun, ranges, fillOpacity, fillPos, fillNeq, abs 
     }
 }
 
-function addRimSumm(plot, fun, left, right, a, color = 6, move = Math.PI / 16, st_v=0) {
+function addRimSumm(plot, fun, left, right, a, color = 6, move = Math.PI / 8, st_v=0) {
     function checkMax(x) {
         d = (x - Math.PI) % (Math.PI * 2);
         e = 0.001;
@@ -84,7 +84,7 @@ function rimSumm(fun, left, right, n) {
 var S = "условно сходится";
 var text0 = "график sinx/(x^a) на [PI/4,+inf] при a = ";
 
-let e = 0.000001, maxright = 50;
+let e = 0.000001, maxright = 75;
 
 function changeRange(value) {
     p1.removeAll();
@@ -119,7 +119,7 @@ function addRimSumm3() {
     move = 0.2;
     cons = Math.pow(2, -move);
     x = move;
-    for (var b = cons; x < maxright * 3; x += move) {
+    for (var b = cons; x < maxright * 2; x += move) {
         RimMiddleSum += rimSumm(mathFunction2,  b, xPrev, 20);
         x1 = b;
         y1 = RimMiddleSum;
@@ -203,7 +203,7 @@ var range = controls1.addRange(changeRange, text0 + "1 " + S, -2, 5, 0.05, 1);
 
 ////////////////////////////////////третий график///////////////////////////////////////
 var controls3 = new app.Controls(container.addEmptyDiv());
-var p3 = container.addPlot({left:-0.2, right:maxright * 3, top:700, bottom:-0.5, height:400, width:500});
+var p3 = container.addPlot({left:-0.2, right:maxright * 2, top:700, bottom:-0.5, height:400, width:500});
 
 pow2 = 2;
 
